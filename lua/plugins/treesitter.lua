c0902207f6a5d4b64dd5ef8cebd44391c4034457
Thomás Freire <thomas.fsr@gmail.com>
2025-10-09 14:09:25 -0300

    update treesitter language support

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,8 @@ return {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, buil
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = {"lua", "python", "javascript", "typescript", "zig", "go", "css", "html"},
+			ensure_installed = {"lua", "python", "javascript", "typescript", 
+        "zig", "go", "css", "html","vim", "bash", "markdown", "json", "yaml", "dockerfile"},
 			highlight = {enable = true},
 			indent = {enable = true}
 		}) 
