58a924e6e30667bef54f63a3a34986b8dca0f132
Thomás Freire da Silva Reis <thomas.fsr@gmail.com>
2025-10-17 22:04:33 -0300

    ok

--- lua/config/packs.lua
+++ lua/config/packs.lua
@@ -1,11 +1,6 @@
 vim.pack.add({
   {src = "https://github.com/catppuccin/nvim", name = "catppuccin", },
-  {src = "https://github.com/nvim-mini/mini.comment", name = "mini.comment"}
-})
-
-vim.cmd("colorscheme catppuccin-frappe")
-require("mini.comment").setup(
-  {options = {
+  {src = "https://github.com/nvim-mini/mini.comment", name = "mini.comment", {options = {
     custom_commentstring = nil,
     ignore_blank_line = true,
     start_of_line = true},
@@ -14,5 +9,7 @@ require("mini.comment").setup(
     comment_line = '<C-;>',
     comment_visual = '<C-;>',
     textobject = '<C-;>'}
-  }
-)
+  }}
+})
+vim.cmd("colorscheme catppuccin-frappe")
+require("mini.comment")
