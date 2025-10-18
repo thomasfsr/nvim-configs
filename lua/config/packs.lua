0905ec6183f5582f7ab73be9b1834a514a3da609
Thomás Freire da Silva Reis <thomas.fsr@gmail.com>
2025-10-17 22:12:32 -0300

    ok

--- lua/config/packs.lua
+++ lua/config/packs.lua
@@ -1,5 +1,6 @@
 vim.pack.add({
   {src = "https://github.com/catppuccin/nvim", name = "catppuccin", },
+  {src = "https://github.com/nvim-mini/mini.surround"},
   {src = "https://github.com/nvim-mini/mini.comment", name = "mini.comment", {options = {
     custom_commentstring = nil,
     ignore_blank_line = true,
