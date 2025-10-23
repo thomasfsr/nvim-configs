b36894455c927e1b9d03928f3eaea7489aa2ed0a
Thomás Freire da Silva Reis <thomas.fsr@gmail.com>
2025-10-23 19:14:47 -0300

    buffer next and buffer previous keymap <C-n and p>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,6 @@
 -- move the cursor to another window
-vim.keymap.set('n', '<c-n>', ':bnext<CR>')
-vim.keymap.set('n', '<c-n>', ':bnext<CR>')
+vim.keymap.set('n', '<c-n>', ':bn<CR>') --buffer next
+vim.keymap.set('n', '<c-p>', ':bp<CR>') -- buffer previous
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
