5dea1c998ae054e02daafe92e1ff1889689df501
Thomás Freire <thomas.fsr@gmail.com>
2025-10-24 14:04:40 -0300

    toggle terminal with shift + t

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,7 +2,7 @@ return {
   'akinsho/toggleterm.nvim', version = "*",
   config = function ()
     require('toggleterm').setup({
-      open_mapping = [[<c-t>]]
+      open_mapping = [[<S-t>]]
     })
   end
 }
