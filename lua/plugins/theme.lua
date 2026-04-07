771bb860ace7f6caeee843794329b165cbad0d47
Thomás Freire <thomas.fsr@gmail.com>
2026-04-07 18:26:32 -0300

    ok

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,13 +1,23 @@
+-- return {
+-- 	"neanias/everforest-nvim",
+-- 	priority = 1000,
+-- 	config = function()
+-- 		require("everforest").setup({
+-- 			background = "hard",
+-- 			italics = true,
+-- 			ui_contrast = "high",
+-- 			-- transparent_background_level = 0.5,
+-- 		})
+-- 		vim.cmd.colorscheme("everforest")
+-- 	end,
+-- }
 return {
-	"neanias/everforest-nvim",
+	"folke/tokyonight.nvim",
+	lazy = false,
 	priority = 1000,
+	opts = {},
 	config = function()
-		require("everforest").setup({
-			background = "hard",
-			italics = true,
-			ui_contrast = "high",
-			-- transparent_background_level = 0.5,
-		})
-		vim.cmd.colorscheme("everforest")
+		--color styles: night, storm, moon and day
+		vim.cmd.colorscheme("tokyonight-night")
 	end,
 }
