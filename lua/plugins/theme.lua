2c48e44a9ec48dc9facfb1c1eb4fce265496a817
Thomas <thomas.fsr@gmail.com>
2026-06-25 16:20:47 -0300

    theme changed to rose default

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -20,6 +20,7 @@ return {
 				ui_contrast = "high",
 				-- transparent_background_level = 0.5,
 			})
+			-- vim.cmd("colorscheme everforest")
 		end,
 	},
 	{
@@ -35,7 +36,7 @@ return {
 		"rose-pine/neovim",
 		name = "rose-pine",
 		config = function()
-			vim.cmd("colorscheme rose-pine-moon")
+			vim.cmd("colorscheme rose-pine")
 		end,
 	},
 }
