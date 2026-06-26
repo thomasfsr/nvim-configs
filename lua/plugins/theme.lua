3dc44ac44b31208f8a005e0ca0a40f59aff5f62c
Thomas <thomas.fsr@gmail.com>
2026-06-26 08:31:09 -0300

    ok

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,42 +1,51 @@
 return {
+	-- {
+	-- 	"folke/tokyonight.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	opts = {},
+	-- 	config = function()
+	-- 		--color styles: night, storm, moon and day
+	-- 		vim.cmd.colorscheme("tokyonight-night")
+	-- 	end,
+	-- },
 	{
-		"folke/tokyonight.nvim",
-		-- lazy = false,
-		-- priority = 1000,
-		-- opts = {},
-		-- config = function()
-		-- 	--color styles: night, storm, moon and day
-		-- 	vim.cmd.colorscheme("tokyonight-night")
-		-- end,
-	},
-	{ "Mofiqul/dracula.nvim" },
-	{
-		"neanias/everforest-nvim",
+		"catppuccin/nvim",
+		name = "catppuccin",
 		priority = 1000,
 		config = function()
-			require("everforest").setup({
-				background = "hard",
-				italics = true,
-				ui_contrast = "high",
-				-- transparent_background_level = 0.5,
-			})
-			-- vim.cmd("colorscheme everforest")
+			vim.cmd.colorscheme("catppuccin-frappe")
 		end,
 	},
-	{
-		"loctvl842/monokai-pro.nvim",
-		lazy = false,
-		priority = 1000,
-		-- config = function()
-		-- require("monokai-pro").setup()
-		-- vim.cmd.colorscheme("monokai-pro-spectrum")
-		-- end,
-	},
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		config = function()
-			vim.cmd("colorscheme rose-pine")
-		end,
-	},
+
+	-- { "Mofiqul/dracula.nvim" },
+	-- {
+	-- 	"neanias/everforest-nvim",
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		require("everforest").setup({
+	-- 			background = "hard",
+	-- 			italics = true,
+	-- 			ui_contrast = "high",
+	-- 			-- transparent_background_level = 0.5,
+	-- 		})
+	-- 		-- vim.cmd("colorscheme everforest")
+	-- 	end,
+	-- },
+	-- {
+	-- 	"loctvl842/monokai-pro.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	-- config = function()
+	-- 	-- require("monokai-pro").setup()
+	-- 	-- vim.cmd.colorscheme("monokai-pro-spectrum")
+	-- 	-- end,
+	-- },
+	-- {
+	-- 	"rose-pine/neovim",
+	-- 	name = "rose-pine",
+	-- 	config = function()
+	-- 		vim.cmd("colorscheme rose-pine")
+	-- 	end,
+	-- },
 }
