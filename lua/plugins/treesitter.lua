59d99d6cd9cb33ba9ee0206a8081293059f5e0cf
Thomas Freire da Silva Reis <thomas.fsr@gmail.com>
2026-07-18 10:32:39 -0300

    now works with blade.php

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -36,18 +36,18 @@ return {
 				"vue",
 				"regex",
 			},
-			highlight = {
-				enable = true,
-				disable = function(lang, buf)
-					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-					if ok and stats and stats.size > 100 * 1024 then
-						return true
-					end
-					local ts_ok = pcall(vim.treesitter.get_parser, buf, lang)
-					return not ts_ok
-				end,
-			},
+			highlight = { enable = true },
 			indent = { enable = true },
+		})
+		-- vim.api.nvim_create_autocmd("FileType", {
+		-- 	callback = function(args)
+		-- 		pcall(vim.treesitter.start, args.buf)
+		-- 	end,
+		-- })
+		vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
+			callback = function(args)
+				pcall(vim.treesitter.start, args.buf)
+			end,
 		})
 	end,
 }
