2f7132515865d5d2974a5756c0ee386b32ece992
Thomas Freire da Silva Reis <thomas.fsr@gmail.com>
2026-07-25 00:39:26 -0300

    ok

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -40,11 +40,6 @@ return {
 				"xml",
 			},
 		})
-		-- vim.api.nvim_create_autocmd("FileType", {
-		-- 	callback = function(args)
-		-- 		pcall(vim.treesitter.start, args.buf)
-		-- 	end,
-		-- })
 		vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
 			callback = function(args)
 				pcall(vim.treesitter.start, args.buf)
