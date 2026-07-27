137dba76edb1689a220d7cbcd7ead2e85a4a34fe
Thomas <thomas.fsr@gmail.com>
2026-07-27 13:46:58 -0300

    so mudanca de cor do catppuccin

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -14,7 +14,7 @@ return {
 		name = "catppuccin",
 		priority = 1000,
 		config = function()
-			vim.cmd.colorscheme("catppuccin-frappe")
+			vim.cmd.colorscheme("catppuccin-macchiato")
 		end,
 	},
 
